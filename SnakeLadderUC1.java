e1f9cac696705dc8a3b5e3975c24363bf1a50178
Sourav <souravkhare34@gmail.com>
2022-10-18 23:37:50 +0530

    Player position at 0

---
+++ SnakeLadderUC1.java
@@ -0,0 +1,12 @@
+package com.snakeandladder;
+
+public class SnakeLadderUC1 {
+    public static void main(String[] args) {
+
+        //Printing initial position
+
+        int position =0 ;
+        System.out.println("Player is at Position "+position);
+
+    }
+}
